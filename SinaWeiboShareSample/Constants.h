6165eca4521f106aaa3747f295939eb4edf87ab5
xnmdyjj <xnmdyjj@gmail.com>
2012-09-18 00:23:15 +0800

    add your app key and secret in this file

--- SinaWeiboShareSample/Constants.h
+++ SinaWeiboShareSample/Constants.h
@@ -9,8 +9,8 @@
 #ifndef SinaWeiboShareSample_Constants_h
 #define SinaWeiboShareSample_Constants_h
 
-#define kWBSDKDemoAppKey @"1931678697"
-#define kWBSDKDemoAppSecret @"02602ef1661bab4af7b3a3e3a589e514"
+#define kWBSDKDemoAppKey @"your app key"
+#define kWBSDKDemoAppSecret @"your app secret"
 
 #ifndef kWBSDKDemoAppKey
 #error
